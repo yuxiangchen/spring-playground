ab8b80501f48cadfbdab5184859570f21af4f046
trainer8 <trainer8@trainer8s-MacBook-Pro.local>
2017-03-28 09:02:36 -0600

    initial commit unit1 yuxiangchen

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Created by trainer8 on 3/27/17.
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello from Spring!";
+    }
+}
